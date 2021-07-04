163f8e113774016989c77c64166a8e03948c8cf2
SaidHadad <saiddavid.hadad@gmail.com>
2021-07-04 14:49:36 -0400

    Manager Object
    Created TDD for manager object

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,1 +1,15 @@
-const Manageer = require('../lib/Manager.js')
+const { test, expect } = require('@jest/globals');
+const Manager = require('../lib/Manager.js')
+
+test('creates manager object', () => {
+  const manager = new Manager('Fulano', 'fulanosutano@gmail.com', 'Manager', 101);
+  expect(manager.email).toBe('fulanosutano@gmail.com');
+  expect(manager.office).toEqual(expect.any(Number));
+  expect(manager.role).toBe('Manager');
+  expect(manager.name).toBe('Fulano');
+});
+
+test('gets the office number of the manager', () => {
+  const manager = new Manager('Fulano', 'fulanosutano@gmail.com', 'Manager', 101);
+  expect(manager.getOffice()).toEqual(expect.any(Number));
+});
